59fb1ad27cc2a8faa7ca9b8c6971fc432b461404
Mai <maiayman888@gmail.com>
2021-04-28 22:38:57 +0200

    inilial commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://mai:mai.2207@cluster0.gthna.mongodb.net/quizDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser : true , 
+    useCreateIndex : true,
+    useFindAndModify:false
+})
+
+
+ 
+
---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+require('./db/mongoose')
+const routes = require('./routes') // includes the routes.js file
+const cors = require('cors') // includes cors module
+
+require('dotenv').config()
+
+app.use(cors()) // We're telling express to use CORS
+app.use(express.json()) // we need to tell server to use json as well
+app.use(routes) // tells the server to use the routes in routes.js
+
+
+
+
+const port = process.env.PORT ||3000
+app.listen(port, () => {
+    console.log("The api is running..." +port)
+})
